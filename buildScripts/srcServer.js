11c3456d33ff26912c5caea29c74b67f6cbb1a47
divya002 <divyaprakash002@gmail.com>
2017-07-16 15:47:09 +0530

    http calls features added

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -11,20 +11,42 @@ app.use(require('webpack-dev-middleware')(compiler,{
     publicPath:config.output.publicPath
 }));
 
-app.get("/",function(req,res)
+app.get('/',function(req,res)
+{
+    res.sendFile(path.join(__dirname,'../src/index.html'));
+}
+);
+
+app.get('/child',function(req,res)
 {
     res.sendFile(path.join(__dirname,'../src/child.html'));
 }
 );
-const port=3001;
+
+app.get('/inform',function(req,res)
+{
+    res.sendFile(path.join(__dirname,'../src/inform.html'));
+}
+);
+
+app.get('/users',function(req,res){
+res.json ([
+{"id":1,"firstName":"ram","lastName":"Smith","email":"div2@gmail.com"},
+{"id":2,"firstName":"ram","lastName":"Smith","email":"div2@gmail.com"},
+{"id":3,"firstName":"ram","lastName":"Smith","email":"div2@gmail.com"},
+
+]
+);
+});
+const port=3000;
 app.listen(port,function(err)
 {
 if(err)
 {
-    console.log(err);
+    console.log(err);//eslint-disable-line no-console
 }
 else
 {
-    open('http://localhost:'+port);
+    open('http://localhost:' + port);
 }
 });
